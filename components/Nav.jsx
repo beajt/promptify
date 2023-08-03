de57319b7c5cc3e0b35a8ad2235c411afc2ae9dd
beatuano <beatuano@gmail.com>
2023-08-02 20:15:45 -0400

    authentication added

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,20 @@ import { useState, useEffect } from "react";
 import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Nav = () => {
-    const isUserLoggedIn = true;
+    const {data: session} = useSession();
 
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
-        const setProviders = async () => {
+        const setUpProviders = async () => {
             const response = await getProviders();
             
             setProviders(response)
         }
-    })
+
+        setUpProviders();
+    }, [])
 
   return (
     <nav className="flex between w-full mb-16 pt-3">
@@ -34,7 +36,7 @@ const Nav = () => {
 
         {/* Desktop Navigation */}
         <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
             <div className="flex gap-3 md:gap-5">
                 <Link href="/create-prompt"
                 className="black_btn">
@@ -62,7 +64,7 @@ const Nav = () => {
                 key={provider.name}
                 onClick={() => signIn(provider.id)}
                 className="black_btn">
-                    
+                Sign In
                 </button>
             ))}
             </>
@@ -71,7 +73,7 @@ const Nav = () => {
 
         {/* Mobile Navigation */}
         <div className="sm:hidden flex relative">
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className="flex">
                     <Image 
                     src="/assets/images/logo.svg"
