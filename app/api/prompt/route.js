dfb3d5009f3603bce069c3295341ad74e2378bf0
beatuano <beatuano@gmail.com>
2023-08-15 00:07:24 -0400

    added prompt route.js

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -10,6 +10,6 @@ export const GET = async (request) => {
     return new Response(JSON.stringify(prompts), {
         status: 200})
   } catch (error) {
-    return new Response ("Failed to fetch all prompts", {status: 200})
+    return new Response ("Failed to fetch all prompts", {status: 500})
   }
 };
