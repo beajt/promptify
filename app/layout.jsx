84b00078c379afc02aa8569af8ab37cad817785a
beatuano <beatuano@gmail.com>
2023-08-17 01:05:05 -0400

    icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,20 +10,23 @@ export const metadata = {
 
 export const RootLayout = ({children}) => {
   return (
-    <html lang ="en">
-        <body>
-            <Provider>
-                <div className='main'>
-                    <div className='gradient' />
-                </div>
-                <main className='app'>
-                    <Nav />
-                    {children}
-                </main>
-            </Provider>
-        </body>
+    <html lang="en">
+      <head>
+        <link rel="icon" href="public/assets/images/logo.ico" />
+      </head>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
+  );
 }
 
 export default RootLayout;
